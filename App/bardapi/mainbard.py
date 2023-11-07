33476d94c403ed7cc2cefd7f38d6709f3a4c0b15
swstone2 <146785949+swstone2@users.noreply.github.com>
2023-11-07 01:14:01 -0500

    Create mainbard.py

---
+++ App/bardapi/mainbard.py
@@ -0,0 +1,23 @@
+import requests
+import json
+ 
+API_KEY = "AIzaSyDVFpBGIk7UyBo9EyD_4-e8oBvNsFT-dSE"
+URL = "https://bard.googleapis.com/v1/generate"
+
+
+def get_bard_response(query):
+    response = requests.post(URL, headers=
+                   {"Authorization": "Bearer " + API_KEY},
+                             json={"query": query})
+    data = json.loads(response.content)
+    return data["text"]
+
+def bardFactcheck(query):
+    response = get_bard_response(query)
+    print("Google Bard Response:")
+    print(response)
+    return response
+ 
+if __name__ == "__main__":
+    query = "Donald Trump is the current President of the USA"
+    bardFactcheck(query)
