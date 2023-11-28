8dacb91dec44c005bdc78485be1dc23c459698b4
swstone2 <146785949+swstone2@users.noreply.github.com>
2023-11-28 08:49:22 -0500

    Change to PaLM
    Had to change to PaLm, Functionally the same just more relaible than using the bard API work around

--- App/bardapi/mainbard.py
+++ App/bardapi/mainbard.py
@@ -1,23 +1,38 @@
-import requests
-import json
- 
-API_KEY = "AIzaSyDVFpBGIk7UyBo9EyD_4-e8oBvNsFT-dSE"
-URL = "https://bard.googleapis.com/v1/generate"
+import pprint
+import google.generativeai as palm
 
+palm.configure(api_key='AIzaSyBDweceTvXjWmBrHo28i1vyWQzaeHUXaSI')
+def run(statement):
+    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    model = models[0].name
+    print(model)
+    #statements to evaluate can only evalute 3 at once
+    preprompt = """
+    Please follow these guidelines to evaluate the following statements and provide sources for your claims:
 
-def get_bard_response(query):
-    response = requests.post(URL, headers=
-                   {"Authorization": "Bearer " + API_KEY},
-                             json={"query": query})
-    data = json.loads(response.content)
-    return data["text"]
+    Use credible and reliable sources, such as academic journals, reputable news organizations, or government websites.
+    Avoid using biased or unreliable sources, such as personal blogs, social media posts, or websites with an agenda.
+    If you cannot find a source to support or contradict a statement, indicate that you were unable to verify the statement.
+    Statements to evaluate:
 
-def bardFactcheck(query):
-    response = get_bard_response(query)
-    print("Google Bard Response:")
-    print(response)
-    return response
- 
+        """
+    prompt = preprompt +statement
+    print(prompt)
+
+    completion = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0,
+        # The maximum length of the response
+        max_output_tokens=800,
+    )
+    #print(completion.result)
+    return completion.result
+def startpalm(query):
+    res = run(query)
+    return res
+
 if __name__ == "__main__":
-    query = "Donald Trump is the current President of the USA"
-    bardFactcheck(query)
+    #enter test query
+    query = "is the earth flat?"
+    print(startpalm(query))
